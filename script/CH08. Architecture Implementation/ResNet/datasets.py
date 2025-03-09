d4d7a948ecaa479e4e5038b85cdd4598daeda76d
Choi Sang Heon <csh01470@gmail.com>
2025-03-10 00:06:38 +0900

    `2025-03-10`

--- script/CH08. Architecture Implementation/ResNet/datasets.py
+++ script/CH08. Architecture Implementation/ResNet/datasets.py
@@ -1,5 +1,5 @@
+import torch
 import torchvision
-from torch.utils.data import DataLoader
 
 # Data loader
 def load_dataset(batch_size:int, mode:'str') -> tuple :
@@ -15,14 +15,14 @@ def load_dataset(batch_size:int, mode:'str') -> tuple :
                 torchvision.transforms.RandomCrop(32, padding=4),
                 torchvision.transforms.RandomHorizontalFlip(),
                 torchvision.transforms.ToTensor(),
-                torchvision.transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010))
+                torchvision.transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)) # WHY?
             ]
         )
         train = torchvision.datasets.CIFAR10(root='data', download=True, train=True, transform=img_tf_train)        
     elif mode == 'eval' :
         train = torchvision.datasets.CIFAR10(root='data', download=True, train=True, transform=img_tf_eval)
     test = torchvision.datasets.CIFAR10(root='data', download=True, train=False, transform=img_tf_eval)
-    train_loader = DataLoader(dataset=train, batch_size=batch_size, num_workers=0)
-    test_loader = DataLoader(dataset=test, batch_size=batch_size, shuffle=False, num_workers=0)
+    train_loader = torch.utils.data.DataLoader(dataset=train, batch_size=batch_size, num_workers=0)
+    test_loader = torch.utils.data.DataLoader(dataset=test, batch_size=batch_size, shuffle=False, num_workers=0)
     output = (train_loader, test_loader)
     return output
